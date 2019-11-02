58e0fbb4d3b6ea091443b14bc02462ce3f29f214
kIshikura-F <kazuki.suka37@gmail.com>
2019-11-02 15:32:50 +0900

    グループ作成機能の実装・サイドメニューにグループ一覧の表示・編集機能・ルートの変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to:"posts#index"
+  root to:"groups#index"
 
-  resources :users, only: [:edit, :update]
+  resources :users, only: [:index, :edit, :update]
+  resources :groups, only: [:new, :create, :edit, :update]
+    resources :posts, only: [:index]
 end
