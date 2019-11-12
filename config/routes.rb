b90e138456ce537ea5f7c8a71fb000ee05dcaaa6
kIshikura-F <kazuki.suka37@gmail.com>
2019-11-12 18:55:05 +0900

    Revert "ルーティングを追加"
    This reverts commit 3fd7710577dfd27fda4cba2d257f47f3a8809d7e.

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,5 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :edit, :update]
   resources :groups, only: [:index, :new, :create, :edit, :update] do
     resources :posts, only: [:index, :create]
-
-    namespace :api do
-      resources :posts, only: :index, defaults: {format;'json'}
   end
 end
