0100b4dfe7dc8d1ff9f80a002fc1b045a5bdfbe2
kIshikura-F <kazuki.suka37@gmail.com>
2019-11-12 13:13:02 +0900

    曖昧な検索、検索した結果のHTMLを描画

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,5 +1,48 @@
-$(function(){
+$(document).on('turbolinks:load', function(){
+
+  var search_list = $("#user-search-result");
+  var member_list = $("#member-append");
+  function appendUser(user){
+    var html = `<div class="chat-group-user clearfix">
+                  <p class="chat-group-user__name">${user.name}</p>
+                  <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id=${user.id} data-user-name=${user.name}>追加</div>
+                </div>`;
+    search_list.append(html);
+  }
+
+  function appendNoUser(){
+    var html = `<div class="chat-group-user clearfix">
+                  <p class="chat-group-user__name">一致するユーザーが見つかりません</p>
+                </div>`;
+    search_list.append(html);
+  }
+
   $("#user-search-field").on("keyup", function(){
     var input = $("#user-search-field").val();
-  });
-});
+    $.ajax({
+      type: 'GET',
+      url: '/users',
+      data: { keyword: input },
+      dataType: 'json'
+    })
+    
+    .done(function(users){
+      if (input.length === 0) {         // フォームの文字列長さが0であれば、インクリメンタルサーチ結果を表示しないようにする
+        $('#user-search-result').empty();
+      }
+      else if (users.length !== 0){     // 値が等しくないもしくは型が等しくなければtrueを返す。
+        $('#user-search-result').empty();
+          users.forEach(function(user){ // users情報をひとつずつとりだしてuserに代入
+            appendUser(user)
+        });
+      }
+      else {
+        appendNoUser();
+      }
+    })
+ 
+    .fail(function() {
+      alert("ユーザー検索に失敗しました");
+    })
+  })
+})
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,10 @@
 class UsersController < ApplicationController
 
   def index
-    respond_to do |format|
+    @users = User.where(['name LIKE ?', "%#{params[:keyword]}%"] ).where.not(id: current_user.id).limit(10)
+    respond_to do |format| 
       format.html
-      format.json
+      format.json 
     end
   end
   
