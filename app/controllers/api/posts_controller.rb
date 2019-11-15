e166b359deff38bcbf4bd5b3d7db6d9eb0fad88a
kIshikura-F <kazuki.suka37@gmail.com>
2019-11-15 11:09:27 +0900

    test

---
+++ app/controllers/api/posts_controller.rb
@@ -0,0 +1,7 @@
+class Api::MessagesController < ApplicationController
+  def index
+    group = Group.find(params[:group_id])
+    last_post_id = params[:id].to_i
+    @posts = group.posts.includes(:user).where("id > #{last_post_id}")
+  end
+end
