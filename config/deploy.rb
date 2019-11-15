47e6cb468d0fd510c782ef380f7d4165111d857e
kIshikura-F <kazuki.suka37@gmail.com>
2019-11-15 12:18:50 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -29,6 +29,7 @@ set :keep_releases, 5
 set :linked_files, %w{ config/secrets.yml }
 
 # デプロイ処理が終わった後、Unicornを再起動するための記述
+after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
   task :restart do
     invoke 'unicorn:restart'
